04cc18a60aaa5133a8f9085de1336c45bcf1d478
Xu Jian <xujian0219@126.com>
2017-01-08 19:22:32 +0800

    新版基本框架完成

---
+++ js/main.js
@@ -0,0 +1,270 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 点击大类过滤出对应的标签+文章:  同步进行高亮.
+  $(".cateNames").click(   function() {
+  // 点击大类: 1. 去除所有大类+标签+文件高亮; 2. 高亮被点击大类 
+      $("#cateDiv li").each(     function() {  $(this).removeClass('active'); });    
+      $("#tagDiv li").each(      function() {  $(this).removeClass('active'); });    
+      $("#filenameDiv li").each( function() {  $(this).removeClass('active'); });      
+      $(this).addClass('active');   
+
+      var clickedCateName = $( this ).children('span').text()
+      // console.log( clickedCateName );
+      // 可以获取 jQuery. 
+      // 隐藏所有tag 显示某类tag
+      $(".tagNames").hide()
+      $("[data-tagcate="+ clickedCateName +"]").show()
+      // 先隐藏所有文件. 显示某类文件
+      $(".postNames").hide()
+      $("[data-cate="+ clickedCateName +"]").show()
+  });
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ↓ 点击标签 过滤出对应的文章. 并进行 当前标签的高亮+当前标签所属大类的高亮.
+  $(".tagNames").click(   function() {
+      $("#cateDiv li").each(     function() {  $(this).removeClass('active'); });    
+      $("#tagDiv li").each(      function() {  $(this).removeClass('active'); });    
+      $("#filenameDiv li").each( function() {  $(this).removeClass('active'); });      
+      $(this).addClass('active'); 
+      var tagCateName = $(this).data('tagcate');
+      // console.log( tagCateName );
+      $("#"+ tagCateName).addClass('active'); 
+
+      var clickedTagName = $( this ).children('span').text()
+      // 先隐藏所有文件. 显示某类文件
+      $(".postNames").hide()
+      $("[data-tag="+ clickedTagName +"]").show()
+
+  });
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ↓ 文件高亮.+对应tag+对应cate 同步高亮.   这里还要显示出 该大类下的标签.而不是显示所有标签
+  $(".postNames").click(   function() {
+      $("#cateDiv li").each(     function() {  $(this).removeClass('active'); });    
+      $("#tagDiv li").each(      function() {  $(this).removeClass('active'); });    
+      $("#filenameDiv li").each( function() {  $(this).removeClass('active'); });      
+      $(this).addClass('active'); 
+
+      var  postTagName = $(this).data('tag' );
+      var postCateName = $(this).data('cate');
+      $(".tagNames").hide()
+      $("[data-tagcate="+ postCateName +"]").show()      
+      $("#"+ postTagName ).addClass('active'); 	
+      $("#"+ postCateName).addClass('active'); 	
+
+  });
+
+
+// 上面是 大类 标签过滤+高亮
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 各种额外功能:   拖动条js  + 方向箭js + pjax  + 回到顶部 + 滚动条高度.
+
+
+// 拖动条. 获取元素左边位置的函数. 要自己写. 不像鼠标位置 直接可以通过clientx 什么的 获取.
+// offsetLeft = 当前元素的外边框 . 到父元素的 里边框的距离.
+// offsetParent = 当前元素 上一级的定位元素....  很麻烦.. 诶 看着办...
+  function getElementLeft(element){
+    var actualLeft = element.offsetLeft;
+    var current = element.offsetParent;
+    while (current !== null){
+      actualLeft += current.offsetLeft;
+      current = current.offsetParent;
+    }
+    return actualLeft;
+  }
+
+  $(function(){
+    <!-- ----------------------------------------------------------------------------------------------- -->
+    // $("#filenameDiv").css("flex-basis","600px");  这个是获取 和 修改div宽度的.    下面是 左线拖动代码
+    // 其实就是 设置 tagDiv 的宽度.  = 鼠标的实时位置 -  鼠标和拖动条直接的微小差距 - tagDiv左边的宽度 - tagDiv的Padding(这个获得元素边距的函数 有个小问题.所以需要减去)
+    // yy 是 鼠标和拖动条之间的微小差距.
+    // realMouse 是鼠标的实时位置.
+    // aa 是tagDiv 左边的宽度
+    lineLeft.onmousedown = function(e) {
+        var disLeft = (e || event).clientX;             
+        // 鼠标按下时鼠标的 X坐标 固定值.
+        var xx = getElementLeft(lineLeft)               
+        // lineLeft 元素 类似clientX的值. 这个函数是自己写的...也是固定值.
+        //console.log("被点击元素的坐标="+ xx);
+        var gapL = disLeft - xx;                          // 这个就是 鼠标到 lineleft 左边框的距离. .
+        console.log("鼠标到linediv 的差距= "+ gapL );    // 也是出来数字 19  不到px的... 整个宽度也就20.这个是对的...
+        //console.log("鼠标实时位置= "+ e.clientX );    // 这里也是对的 ..
+        var tagDivL = getElementLeft(tagDiv);
+            //console.log("tagDiv的左边位置="+ aa);
+
+            document.onmousemove = function(e) {
+              var realMouse = e.clientX;                                     // 鼠标移动的实时x轴值. 变化
+                console.log("实际鼠标位置(不带px)= "+ realMouse);               //  不带px
+              var leftPaddingNum = parseFloat($("#cateDiv").css("padding-left"));  
+              // 这个是 获取cateDiv 的padding. 需要单独减去.. .. 去掉px.只需要数值... 不应该是tagDiv的padding么 -,- 
+              // 懒得调整 就直接减去 多少px 好了....
+                console.log("leftPaddingNum 不带px= "+ leftPaddingNum);       //  不带px 
+                console.log("tagDiv的左边位置="+ tagDivL );
+                console.log("鼠标到linediv 的差距= "+ gapL );
+              var zz = realMouse - tagDivL - leftPaddingNum - gapL    ;               // 鼠标实时坐标 - 一定的差距 =  实时的左边div 的宽度
+                console.log("tagDiv实际实时宽度= "+ zz);                                     // 也不带px
+                $("#tagDiv").css("flex-basis",zz);    // 这里就能实时拖动了.
+
+              // 下面是 鼠标松开的时候触发的事件....
+              document.onmouseup = function() {  
+                document.onmousemove = null;  document.onmouseup = null;   
+              };
+           }
+    };
+    <!-- ----------------------------------------------------------------------------------------------- -->
+    // 下面是 右线拖动代码: fileDiv宽度= 鼠标实时x位置 - fileDiv左边距x - 鼠标到fileDiv右边的微小差距 
+    // 有时候要减去 padding .有时候不用... 看情况..
+    lineRight.onmousedown = function(e) {
+        var Mouse = (e || event).clientX;			
+        console.log("鼠标点击值= "+ Mouse);
+        var line2Width = getElementLeft(lineRight)
+        console.log("右线自定义函数宽度= "+line2Width);
+        var gap = Mouse - line2Width;		
+        console.log("鼠标和右线之间的微小差距.  需要减去= "+gap);
+        // 正常情况下 拖动 gap 是个固定值.
+        var middleWidth = getElementLeft(filenameDiv)
+        //console.log("中间自定义函数宽度= "+ middleWidth);
+        //console.log("\n" );
+            document.onmousemove = function(e) {
+                var realMouse = (e || event).clientX;	
+                var middlePaddingPX = $("#filenameDiv").css("padding");
+                console.log("filenameDiv padding 带px= " +middlePaddingPX);  
+                // 这个是带px的. 需要提取前面的数字部分. padding 是有两边的..
+                var middlePaddingNum = parseFloat(middlePaddingPX);
+                console.log("filenameDiv padding 无px= " +middlePaddingNum);  
+                var realMiddle = realMouse - middleWidth - gap;
+                $("#filenameDiv").css("flex-basis",realMiddle); 
+            document.onmouseup = function() { document.onmousemove = null;  document.onmouseup = null;   };  }
+    };
+  });
+
+// 上面是  拖动条的 js
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 方向箭的js
+
+  $(function button(){
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 判断 所有的都不在.就显示. 不然就隐藏;   隐藏(none) → 就显示左边 3个+两拖动条;  显示(block):→隐藏左边三个+两拖动条 
+    $("#sidebarToggle").click(   function(){ 
+      if ( $("#filenameDiv").css("display") == "none" && $("#tagDiv").css("display") == "none" && $("#cateDiv").css("display") == "none" ) {
+          $("#filenameDiv").show(100);
+          $("#tagDiv").show(100);
+          $("#cateDiv").show(100);
+          $("#lineLeft").show(100);
+          $("#lineRight").show(100);
+         } else {
+          $("#filenameDiv").hide(100);
+          $("#tagDiv").hide(100);
+          $("#cateDiv").hide(100);
+          $("#lineLeft").hide(100);
+          $("#lineRight").hide(100);
+         }
+    });
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // 下面进行 左按钮设计.隐藏顺序 cate>tag>filename.   $('#userNav').hide('slide',{direction:'left'},1000);
+
+    function toLeft() {
+      if      ( $("#cateDiv").css("display") != "none" ) { $("#cateDiv").hide(  100); } 
+      else if ( $("#tagDiv").css("display") != "none"  ) { $("#tagDiv").hide(    100); $("#lineLeft").hide(  100); }
+      else                                               { $("#filenameDiv").hide( 100); $("#lineRight").hide( 100); }
+    }
+
+    function toRight() {
+      if      ( $("#filenameDiv").css("display") == "none" ) { $("#filenameDiv").show(100); $("#lineRight").show(100); } 
+      else if ( $("#tagDiv").css("display") == "none"  )     { $("#tagDiv").show( 100);      $("#lineLeft").show( 100);  }
+      else                                                   { $("#cateDiv").show(100);  }
+    }
+
+    $("#leftBtn").click(   function() { toLeft()  } );
+    $("#rightBtn").click(  function() { toRight() } );
+    // 下面是 右下角的 左右键盘 开控制 显示隐藏的.
+    $(document).keydown(function(event){ 
+            if (event.keyCode == 37){   toLeft();   }
+       else if (event.keyCode == 39){   toRight();  } 
+       else if (event.keyCode == 38){   toTop();    } 
+      }); 
+  });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 pjax 
+$(document).pjax("a", '#contentDiv', { fragment: '#contentDiv', timeout:8000}	);	
+$(document).on('pjax:start', function() { NProgress.start(); });
+$(document).on('pjax:end',   function() { NProgress.done();  });
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 这个是 回到顶部的函数....
+function toTop() {  $("#contentDiv").scrollTop(0); }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 网页加载时候的 滚动条高度的.  有高度 才能有滚动条.
+  function scrollbarHeight(){
+      function getElementTop(element){
+      var actualTop = element.offsetTop;
+      var current = element.offsetParent;
+      while (current !== null){
+        actualTop += current.offsetTop;
+        current = current.offsetParent;
+          }
+      return actualTop;
+     }
+      // xyz是 浏览器的高度; xy是 filenameDiv 距离浏览器上边距的距离;  x 是 CateDiv 和 tagDiv 的距离.(这两个 水平的)
+      // 设置 fileDiv 高度
+      var xyz = document.documentElement.clientHeight;
+      var  xy = getElementTop(filenameDiv);
+      var   x = getElementTop(cateDiv);
+      var xxyy = xyz - xy;
+          $("#filenameUL").css("height",xxyy)
+      // 下面是cate 和 tag 的高度设置..
+      var xxyy2 = xyz - x;
+          $("#catenameUL").css("height",xxyy2)
+          $("#tagnameUL").css("height",xxyy2) 
+  }
+
+  // 网页加载后 浏览器窗口大小改变时候的 滚动条 高度设置 
+  $(function (){  scrollbarHeight()  });
+  // 浏览器窗口大小改变时候 再次改变滚动条高度
+  window.onresize= function(){ scrollbarHeight()  }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 眼睛功能: 显隐文件日期
+  function fileNameDate() {
+    // console.log( $(".fileNameCustonOrder").css("display")   );
+    // 判断 某元素显示隐藏状态. 显示:block  隐藏:none
+
+    var fileNameSortNumStatus = $(".fileNameDate").css("display");
+      if ( fileNameSortNumStatus ==  "none" ) {
+           $("#filenameDivDate").removeClass("fa fa-eye-slash")  ;
+           $("#filenameDivDate").addClass("fa fa-eye");
+           $(".fileNameDate").removeClass("hidden");
+         } else {
+           $("#filenameDivDate").removeClass("fa fa-eye")  ;
+           $("#filenameDivDate").addClass("fa fa-eye-slash");
+           $(".fileNameDate").addClass("hidden");
+         }
+  }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 显示所有的标签和文章
+function showAllTagsandPosts () {
+    $(".tagNames").show()
+    $(".postNames").show()
+    // 清楚 所有的高亮
+    $("#cateDiv li").each(     function() {  $(this).removeClass('active'); });    
+    $("#tagDiv li").each(      function() {  $(this).removeClass('active'); });    
+    $("#filenameDiv li").each( function() {  $(this).removeClass('active'); });    
+
+}
+
+
+
+
+
--- tempCode.md
+++ tempCode.md
@@ -206,3 +206,263 @@ function cateFilenameFlex() {
 
 
 
+
+
+/*
+
+// 这个函数 传入的变量是 cateDiv 里面的 点击某个分类时候的 分类名:   cateClicked
+function catefilter(cateClicked) { showTagsandPosts(cateClicked); delRows(); setActiveCate(cateClicked); }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  选取遍历 catediv 下的 所有li  然后给选定的大类 添加active  点击大类也要清楚 tagDiv 下的所有active....
+function setActiveCate(cateClicked) {
+	// 清除所有大类高亮+设置某个大类
+	$("#cateDiv li").each( function() {  	$(this).removeClass('active');        });
+	$("#"+ cateClicked).addClass('active');
+	$("#"+ cateClicked +"-num").addClass('active');
+	// console.log( $("#"+ cateClicked) )
+	// 下面是清楚 tagDiv 的active 
+	$("#tagDiv li").each( function() {  	$(this).removeClass('active');  	});
+	// 下面是清除 filename 的高亮
+	$("#filenameDiv li").each( function() {  	$(this).removeClass('active');  	});
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 点击大类 过滤出对应的tag 和 filename..  这里  也要进行yml 默认tag 排序的判断
+function showTagsandPosts(cateClicked) {
+	$(".tagsDiv1-allTags").attr("class","tagsDiv1-allTags hidden");       //$(".tagsDiv1-allTags").css("display","none");
+	$(".tagDiv2-catetags").attr("class","tagDiv2-catetags hidden");
+	$("#"+ cateClicked +"-cate").attr("class","tagDiv2-catetags");         // 把这个元素的class 改成 ... 没有hidden的.
+	// 下面是 filenameDiv 的 
+	$(".blog-list-container").attr("class","blog-list-container hidden");
+	$(".blog-list-container2").attr("class","blog-list-container2 hidden");
+	$(".blog-list-container3").attr("class","blog-list-container3 hidden");
+	$("#"+ cateClicked +"-filenames").attr("class","blog-list-container3");   		
+}	
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 根据元素的 内容长度 来删除 空行. 这里要用 正则式 进行筛选. 而不是长度. 正则式要筛选出来 而不是删除...
+// function trimStr(str){return str.replace(/(^\s*)|(\s*$)/g,"");}
+function delRows() {
+		$(".delEmptyrow-C").each(  function(index){
+			var thisALLText = $(this).text();
+			var thisSpanText = $(this).find("span").text();
+			var textSpanLength =　 $(this).find("span").text().length ;
+			//console.log(thisSpanText+"标签长度"+textSpanLength);
+			if ( $(this).find("span").text().length == 0 )	{	$(this).attr("class","delEmptyrow-C hidden");	}
+		});
+	}
+
+// 上面是 cate 过滤
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 tag 过滤
+
+function filter(tag) {
+  setActiveTag(tag);
+  showfilenameDiv2(tag);
+  tagFilenameFlex();
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function showfilenameDiv2(tag){	
+	if( tag == "allPosts") {
+			// 显示 div1 就可以了... 隐藏div2 div3
+			$(".blog-list-container").attr("class","blog-list-container");
+			$(".blog-list-container2").attr("class","blog-list-container2 hidden");	
+			$(".blog-list-container3").attr("class","blog-list-container3 hidden");	
+			$("#cateDiv li").each( function() {  	$(this).removeClass('active');        }); // 移除 cateDiv    下所有的 active...
+		}
+	else{
+			// 先隐藏 div1 ,这个div是要一直隐藏的.
+			$(".blog-list-container").attr("class","blog-list-container hidden");
+			// 再隐藏 div2 全部 .这里有 每个标签下的各自的文章.只需要显示某个标签就行
+			$(".blog-list-container2").attr("class","blog-list-container2 hidden");
+			// 这里 也要排错 div3... 不然你点过大类下某个标签.这里会残留
+			$(".blog-list-container3").attr("class","blog-list-container3 hidden");
+			// 最后显示 div2 下符合条件的id . 
+			$("#"+ tag + "-container").attr("class","blog-list-container2");
+		}
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  选取遍历 tagdiv 下的 所有li  然后给选定的大类 添加active. tag 有好几个的.
+function setActiveTag(tag) {
+	// 显示所有标签是时候 要去除cate + tag + filename的 所有高亮
+	$("#tagDiv li").each( function() {  	$(this).removeClass('active'); 	});       // 移除tagDiv      下所有的 active... 
+	$("#filenameDiv li").each( function() {  	$(this).removeClass('active'); 	});   // 移除filenameDiv 下所有的 active... 
+	$("#"+ tag +"-item").addClass('active');    // 默认情况 排序1 下的 active
+	$("#"+ tag +"-item2").addClass('active');   // 默认情况 排序1 下的 active
+	$("#"+ tag +"-item3").addClass('active');   // 默认情况 排序1 下的 active
+	$("#"+ tag +"-cate22").addClass('active');   //  大类过滤出来的 active
+}
+// 这里只解决了默认情况下的 active...  大类下 还是不行??
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function tagFilenameFlex() {
+	$ (".blog-list-container22").each(function( index ) {
+	   // console.log( index + ": " + $( this ).text()   );               
+	   // console.log( index + ": " + $( this ).children("span").text() + $( this ).children("span").text().length )
+	   // 上面 获取整个元素的内容.  下面获取子元素span. 
+		var orderValue = $( this ).children('span').text();
+		var orderLength = $( this ).children('span').text().length;
+		if ( orderLength != "0" ) {
+			//console.log( orderValue );
+			//console.log( $(this ).css("order") );
+			$(this ).css("order", orderValue); 
+				//console.log( $(this ).css("order") );
+			}
+	});
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 file过滤
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 显隐 文件时间
+function fileNameDate() {
+  // console.log( $(".fileNameCustonOrder").css("display")   );
+  // 判断 某元素显示隐藏状态. 显示:block  隐藏:none
+  var fileNameSortNumStatus = $(".fileNameDate").css("display");
+    if ( fileNameSortNumStatus ==  "none" ) {
+         $("#filenameDivDate").removeClass("fa fa-eye-slash")  ;
+         $("#filenameDivDate").addClass("fa fa-eye");
+         $(".fileNameDate").removeClass("hidden");
+       } else {
+         $("#filenameDivDate").removeClass("fa fa-eye")  ;
+         $("#filenameDivDate").addClass("fa fa-eye-slash");
+         $(".fileNameDate").addClass("hidden");
+       }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 点击某个文件名 高亮: 文件名+ 对应tag + 对应cate ....用 onclick 传入文件名 ...然后取消所有active 然后设置某个active...
+function filenameActiveCT( afileTag,afileCate ) {
+	setActiveCate(afileCate);   // 必须先设置大类高亮. 因为大类高亮会清除所有的 tag/filename 高亮. 在cateFilter 里.
+	setActiveTag(afileTag);     // 第二个执行   函数在tagfilter里
+}
+
+function filenameActiveThis () {
+	var selector = '#filenameDiv li';
+	$(selector).on('click', function(){
+		$(selector).removeClass('active');
+		$(this).addClass('active');
+	});
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 点击文件名. 显示出该文件名类下的所属 tag.. 类似点击某大类.. 这里只需要显示改大类下的标签就可以. 不用显示该大类的 文章....
+function catefilterPart( afilename ) {
+	$(".tagsDiv1-allTags").attr("class","tagsDiv1-allTags hidden");
+	//$(".tagsDiv1-allTags").css("display","none");
+	$(".tagDiv2-catetags").attr("class","tagDiv2-catetags hidden");
+	$("#"+ afilename +"-cate").attr("class","tagDiv2-catetags"); 
+	// 把这个元素的class 改成 ... 没有hidden的.
+		
+delRows();  //这个是 cateFilter.js 下的 删除空行的 函数..
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 显示所有标签和文章
+function showAllTagsandPosts () {
+	$(".tagsDiv1-allTags").attr("class","tagsDiv1-allTags");               //  默认所有标签时间排序
+	$(".tagDiv2-catetags").attr("class","tagDiv2-catetags hidden"); 	   //  大类过滤出来的 tag
+	// 下面是 filenameDiv的if过滤 
+	$(".blog-list-container").attr("class","blog-list-container");
+	$(".blog-list-container2").attr("class","blog-list-container2 hidden"); 
+	$(".blog-list-container3").attr("class","blog-list-container3 hidden"); 
+	// 显示所有标签是时候 要去除cate + tag + filename的 所有高亮
+	$("#cateDiv li").each( function()     { $(this).removeClass('active'); });
+	$("#tagDiv li").each( function()      { $(this).removeClass('active'); });
+	$("#filenameDiv li").each( function() { $(this).removeClass('active'); });
+}
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+// 这个文件是给排序用的.  cateDiv + tagDiv + filenameDiv 名称排序.
+// cateDiv 大类文件数量排序: 也就是隐藏 默认的 custom 排序div 显示num排序div
+  function cateNumfilter() {
+    $("#cateDivFlex").attr("class","CLcateDivFlex hidden");
+    $(".CLcateDiv1-numberSort").attr("class","CLcateDiv1-numberSort");
+  }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 cateDiv 下的 custom 自定义排序.  和onready.js 几乎一样...
+function cateCustomfilter(){		
+  //console.log( $("#ymlCateName").text() );
+  var xx= $("#ymlCateName").text();
+  //console.log(xx);
+  // 成功取到 cateDiv 下的 yml 里的自定义数据(字符串...)   → Web-1,Misc-2,-3
+  var yy = xx.split(",")
+  // console.log(yy);
+  // 用split 把字符串 通过指定的分割符号 变为 数组  → ["Web-1", "Misc-2", "-3"]
+  //console.log(yy.length);   // 取得数组的长度,用于循环
+  for (i=0; i<yy.length; i++) {
+           // alert(yy[0]); 
+           // 这个会显示出 第一个数据  Web-1.
+          // 把这个数据再次分割. 用 - 作为分隔符
+          var temp = yy[i].split('^');
+              //alert(temp[0]); 
+              //alert(temp[1]);
+              var tempName = temp[0];
+              var tempOrder = temp[1];
+
+          $("#"+ tempName ).css("order",tempOrder); 
+
+          // 然后这个 Web就可以拿来 作为 cateDiv 里面对应的id了. 把这个id 的 flex order 改成 数字.
+          // 这里顺序就修改成功了....
+          // 下面就是怎么实现 一载入就 修改顺序....
+      }
+
+    $(".CLcateDivFlex").attr("class","C.cateDivFlex");
+    // 处理加载主页时候. 会闪的问题...  先隐藏掉默认排序... 等js排序好之后再显示就可以了..
+    $(".CLcateDiv1-numberSort").attr("class","CLcateDiv1-numberSort hidden");
+    // 隐藏掉 num排序的div
+}
+
+
+
+  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 下面是 tagDiv 的排序..
+// 字母排序: →     隐藏 time + num div... ; 显示abcdiv   这里还要隐藏 tag 过滤出来的那个div
+function tagAbcfilter(){
+    $(".tagDiv2-catetags").attr("class","tagDiv2-catetags hidden");        //隐藏 tag过滤出来的某类下的标签 
+    $("#alltagsContainer").attr("class","tagsDiv1-allTags hidden");        //隐藏 time div
+    $("#tagDiv1-numberSort").attr("class","CLtagDiv1-numberSort hidden");  //隐藏  num div
+    $("#tagDiv1-letterSort").attr("class","tagDiv1-letterSort");           //显示  abc div
+}
+
+// Time 排序: → 隐藏abc + num ; 显示time
+function tagTimefilter(){
+    $(".tagDiv2-catetags").attr("class","tagDiv2-catetags hidden");        //隐藏 tag过滤出来的某类下的标签   
+    $("#tagDiv1-numberSort").attr("class","CLtagDiv1-numberSort hidden");  //隐藏  num div
+    $("#tagDiv1-letterSort").attr("class","tagDiv1-letterSort hidden");    //隐藏  abc div
+    $("#alltagsContainer").attr("class","tagsDiv1-allTags");               //显示 time div    
+}
+
+// 数量排序: → 隐藏 abc + time ; 显示num 
+function tagNumfilter(){
+    $(".tagDiv2-catetags").attr("class","tagDiv2-catetags hidden");        //隐藏 tag过滤出来的某类下的标签   
+    $("#alltagsContainer").attr("class","tagsDiv1-allTags hidden");        //隐藏 time div    
+    $("#tagDiv1-letterSort").attr("class","tagDiv1-letterSort hidden");    //隐藏  abc div
+    $("#tagDiv1-numberSort").attr("class","CLtagDiv1-numberSort");         //显示  num div
+}
+
+
+
+
+
+
+
+
