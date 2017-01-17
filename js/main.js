189094dfa281112cf9ad8112f2533420b55f0433
Xu Jian <xujian0219@126.com>
2017-01-17 10:34:48 +0800

    跳转done

--- js/main.js
+++ js/main.js
@@ -323,20 +323,26 @@ function scrollbarHeight(){
       // xyz是 浏览器的高度; xy是 filenameDiv 距离浏览器上边距的距离;  x 是 CateDiv 和 tagDiv 的距离.(这两个 水平的)
       // 设置 fileDiv 高度
       var xyz = document.documentElement.clientHeight;
-      var  xy = getElementTop(filenameDiv);
+      var  xy = getElementTop(filenameUL);
+      console.log(xy);
       var   x = getElementTop(cateDiv);
       var xxyy = xyz - xy;
+      // 这里还是直接减去 顶部栏目的50 +  文件名上固定栏目的.18 得了..
           $("#filenameUL").css("height",xxyy)
       // 下面是cate 和 tag 的高度设置..
       var xxyy2 = xyz - x;
           $("#catenameUL").css("height",xxyy2)
           $("#tagnameUL").css("height",xxyy2) 
+                //alert("xxyy="+xxyy);
+                //alert("xxyy2="+xxyy2);
+
   }
 
   // 网页加载后 浏览器窗口大小改变时候的 滚动条 高度设置 
   $(function (){  scrollbarHeight()  });
   // 浏览器窗口大小改变时候 再次改变滚动条高度
-  window.onresize= function(){ scrollbarHeight()  }
+  window.onresize= function(){ scrollbarHeight()  };
+
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
